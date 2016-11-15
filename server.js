6a746ee66fbeb87c97bb0b3dab16156aeb92aebf
KERTZ <CRKERT@monsanto.com>
2016-11-15 12:17:23 -0600

    fixed pg connection bugs

--- server.js
+++ server.js
@@ -14,18 +14,55 @@ var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
-var db_config = require('./server/config/database.json'); //connect to pg server on aws
 
 // configuration ===============================================================
-var pool = new pg.Pool(db_config);
+var db_conn_string = require('./server/config/database.json')
+//load connection config file
+// Example database.config file
+// {
+//	"user": "my_username", 
+//	"database": "my_database_name",
+//	"password": "my_password",
+//	"host": "localhost",
+//	"port": 5432,
+//	"max": 10,
+//	"idleTimeoutMills": 30000
+// }
+
+//---------------------------------------------------
+// Connect to our pg database using config .json file
+//---------------------------------------------------
+
+//this initializes a connection pool
+//it will keep idle connections open for period determined in the config file
+//and set a limit of maximum idle clients
+var pool = new pg.Pool(db_conn_string);
+
+// to run a query we can acquire a client from the pool
+// run a query on the client, and then return the client to the pool
+// scripts borrowed from npmjs pg package docs
+
 pool.connect(function(err, client, done){
 	if(err){
-		return console.error('error fetching client from pool', err);
-	}
-	done();
-}); // connect to our database
+		return console.error('error fectching client from pool', err);
+}
+	// Return current time to verify connection success. 
+	client.query('SELECT NOW() AS "theTime"', function(err,result){
+		done();
+		
+		if(err){
+			return console.error('Error running startup query', err);
+		}
+		console.log('Connected to ', client.database, ' at ', result.rows[0].theTime,' on port ', client.port);
+		client.end();
+	});
+});
 
-require('./server/config/passport.js')(passport); // pass passport for configuration
+pool.on('error', function (err, client){
+	console.error('idle client error', err.message, err.stack);
+});
+
+//require('./server/config/passport.js')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(logger('dev')); // log every request to the console
